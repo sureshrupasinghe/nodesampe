30b742ccb44919897423cfd1b65cd60f2fcb4e62
sureshrupasinghe <45166891+sureshrupasinghe@users.noreply.github.com>
2018-12-04 13:17:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-    response.send('Hello World...!!! Suresh');
+    response.send('Hello World...!!! Suresh Node');
 });
 
 app.listen(app.get('port'), function() {
