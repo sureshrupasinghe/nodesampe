ba122f84557b0b9022efd62f64d6431d11fabcc2
sureshrupasinghe <45166891+sureshrupasinghe@users.noreply.github.com>
2018-12-17 20:12:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-    response.send('Wercker + Heroku + Node + Tenable Sample Application');
+    response.send('Wercker + Heroku + Node + Tenable Sample Application + 2018');
 });
 
 app.listen(app.get('port'), function() {
